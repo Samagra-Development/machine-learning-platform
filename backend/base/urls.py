0f3504b23da6b882db519983990a55322747b158
pSN0W <08yaksh08@gmail.com>
2022-08-07 04:02:09 +0530

    Added endpoint to create a model

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('datasets',views.get_datasets,name='datasets'),
     path('datasets/<str:pk>',views.get_features,name='features'),
     path('models',views.get_all_model,name='model'),
+    path('models/create',views.create_model,name='create_model'),
 ]
